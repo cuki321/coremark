6190863180e0471280acc1b439b497b1f72aa731
cuki321 <cuki321@yahoo.com>
2014-10-25 11:20:54 -0700

    na

---
+++ firmware/examples/sketch_coremark.ino
@@ -0,0 +1,14 @@
+//#define COREMARK_ARDUINO 1
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+  //delay(4000);
+  //Serial.print("Testing");  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
